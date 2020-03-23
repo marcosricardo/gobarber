ae1c9969fb6cf1b40588a797ea720655ed724931
marcosricardo <mricardoalves99@gmail.com>
2020-03-22 21:32:31 -0300

    Setup sequelize

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,10 +1,10 @@
-import { Router } from "express";
+import  Router  from "express";
 
 const routes = new Router();
 
 routes.get("/", (req, res) => {
     return res.json({
-        message: "Go barber - back end"
+        message: "Go barber - back end - bootcamp"
     })
 })
 
