a834a3867268bbcea86e4c495af23e03b9d5156d
marcosricardo <mricardoalves99@gmail.com>
2020-03-26 20:10:40 -0300

    Conecta com o mongodb

--- back-end/src/database/index.js
+++ back-end/src/database/index.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-
+import mongoose from 'mongoose';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
@@ -9,15 +9,25 @@ import databaseConfig from '../config/database';
 const models = [User, File, Appointment];
 
 class Database {
-    constructor(){
+    constructor() {
         this.init();
+        this.mongo();
     }
 
-    init(){
+    init() {
         this.connection = new Sequelize(databaseConfig);
         models
-        .map(model => model.init(this.connection))
-        .map(model => model.associate && model.associate(this.connection.models));
+            .map(model => model.init(this.connection))
+            .map(model => model.associate && model.associate(this.connection.models));
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            'mongodb://localhost:27017/gobarber', {
+                useNewUrlParser: true,
+                useFindAndModify: true
+            }
+        );
     }
 }
 
