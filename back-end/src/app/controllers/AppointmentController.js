a830e187aac8fee1376736ec99c3086981fb3e32
marcosricardo <mricardoalves99@gmail.com>
2020-03-27 19:20:12 -0300

    Sending email when user cancels appointment

--- back-end/src/app/controllers/AppointmentController.js
+++ back-end/src/app/controllers/AppointmentController.js
@@ -11,29 +11,33 @@ import {
     subHours
 } from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
 
     async index(req, res) {
-        const { page = 1 } = req.query;
+        const {
+            page = 1
+        } = req.query;
 
         const appointments = await Appointment.findAll({
-            where: { user_id:req.userId, canceled_at: null},
+            where: {
+                user_id: req.userId,
+                canceled_at: null
+            },
             order: ['date'],
             attributes: ['id', 'date'],
-            limit:20,
+            limit: 20,
             offset: (page - 1) * 20,
             include: [{
-                model : User,
+                model: User,
                 as: 'provider',
                 attributes: ['id', 'name'],
-                include: [
-                    {
-                        model:File,
-                        as:'avatar',
-                        attributes: ['id', 'path', 'url']
-                    }
-                ]
+                include: [{
+                    model: File,
+                    as: 'avatar',
+                    attributes: ['id', 'path', 'url']
+                }]
             }]
         });
 
@@ -109,23 +113,40 @@ class AppointmentController {
 
         const user = await User.findByPk(req.userId);
 
-        const formattedDate = format(hourStart, "'dia' dd 'de' MMMM', às' H:mm'h ", { locale: pt });
+        const formattedDate = format(hourStart, "'dia' dd 'de' MMMM', às' H:mm'h ", {
+            locale: pt
+        });
 
         /**
          * Notify appointment provider
          */
-         await Notification.create({
+        await Notification.create({
             content: `Novo agendamento de ${user.name} para ${formattedDate}`,
             user: provider_id
-         });
+        });
 
         return res.json(appointment);
     }
 
-    async delete(req, res){
-        const appointment = await Appointment.findByPk(req.params.id);
+    async delete(req, res) {
+        const appointment = await Appointment.findByPk(
+            req.params.id, {
+                include: [
+                    {
+                        model: User,
+                        as: 'provider',
+                        attributes: ['name', 'email']
+                    },
+                    {
+                        model: User,
+                        as: 'user',
+                        attributes: ['name', 'email']
+                    }
+                ]
+            }
+        );
 
-        if(appointment.user_id !== req.userId){
+        if (appointment.user_id !== req.userId) {
             return res.status(401).json({
                 error: 'You don\'t have permission to cancel this appointment.'
             });
@@ -133,7 +154,7 @@ class AppointmentController {
 
         const dateWithSub = subHours(appointment.date, 2);
 
-        if(isBefore(dateWithSub, new Date())){
+        if (isBefore(dateWithSub, new Date())) {
             return res.status(401).json({
                 error: 'You can only cancel appointments 2 hours in advance.'
             });
@@ -142,6 +163,19 @@ class AppointmentController {
         appointment.canceled_at = new Date();
 
         await appointment.save();
+
+        await Mail.sendMail({
+            to: `${appointment.provider.name} <${appointment.provider.email}>`,
+            subject: 'Agendamento cancelado',
+            template: 'cancellation',
+            context: {
+                provider: appointment.provider.name,
+                user: appointment.user.name,
+                date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h ", {
+                    locale: pt
+                }),
+            }
+        });
 
         return res.json(appointment);
     }
---
+++ back-end/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
+    auth: {
+      user: "5c70b1c92a3e8c",
+      pass: "0484a545dab470"
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>'
+    },
+};
