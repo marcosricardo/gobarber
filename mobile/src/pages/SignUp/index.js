b7861079a9b189ba551866f8f79f1784ef155afd
marcosricardo <mricardoalves99@gmail.com>
2020-03-30 01:26:41 -0300

    Page SignIn and SignUp

--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,9 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Image } from 'react-native';
+import Background from '~/components/Background';
+import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles';
+import logo from '~/assets/logo.png';
 
-// import { Container } from './styles';
+export default function SignUp({ navigation })  {
+    return (
+      <Background>
+        <Container>
+            <Image source={logo} />
+            <Form>
+                <FormInput
+                  icon="person-outline"
+                  autoCorrect={false}
+                  autoCapitalize="none"
+                  placeholder="Nome completo"
+                />
 
-export default class SignUp extends Component {
-  render() {
-    return  <Background><Text>SignUp</Text></Background>;
-  }
+                <FormInput
+                  icon="mail-outline"
+                  keyboardType="email-address"
+                  autoCorrect={false}
+                  autoCapitalize="none"
+                  placeholder="Digite seu e-mail"
+                />
+
+                <FormInput
+                  icon="lock-outline"
+                  secureTextEntry
+                  placeholder="Sua senha secreta"
+                />
+
+                <SubmitButton onPress={() => {}}>
+                  Acessar
+                </SubmitButton>
+            </Form>
+        </Container>
+      </Background>
+    );
 }
