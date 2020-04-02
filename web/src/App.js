6b41440f1abeeaf25e3e762d7aac1945a2fcc805
marcosricardo <mricardoalves99@gmail.com>
2020-04-02 11:57:38 -0300

    Setup store
    Redux saga reducer

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+
 import Routes from './routes';
 import history from './services/history';
-import { Router } from 'react-router-dom';
+
 import './config/ReactotronConfig'
 import GlobalStyle from './styles/global';
+import store from './store';
 
 function App() {
   return (
-        <Router history={history}>
-            <Routes />
-            <GlobalStyle />
-        </Router>
+        <Provider store={store}>
+            <Router history={history}>
+                <Routes />
+                <GlobalStyle />
+            </Router>
+        </Provider>
       );
 }
 
