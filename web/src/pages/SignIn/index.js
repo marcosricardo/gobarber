84cd2a05a622266e75de25d0fa74416c13f5694a
marcosricardo <mricardoalves99@gmail.com>
2020-04-02 11:56:51 -0300

    Pages SignIn and SignUp

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+    email: Yup.string().email('Insira um e-mail válido').required('O e-mail é obrigatório'),
+    password: Yup.string().required('A senha é obrigatória')
+});
 
 export default function SignIn() {
+
+  function handleSubmit(data){
+    console.tron.log(data);
+  }
+
   return (
-   <h6>SignIn</h6>
+      <>
+        <img  src={logo} alt="GoBarber" />
+
+        <Form schema={schema} onSubmit={handleSubmit}>
+            <Input name="email" type="email" placeholder="Seu e-mail" />
+            <Input name="password" type="password" placeholder="Sua senha secreta" />
+
+            <button type="submit">Acessar</button>
+            <Link to="register">Criar conta gratuita</Link>
+        </Form>
+      </>
   );
+
 }
