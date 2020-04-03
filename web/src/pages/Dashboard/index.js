098cbc9a56db9cbdcd0eb1483176512f5b413861
marcosricardo <mricardoalves99@gmail.com>
2020-04-03 03:32:04 -0300

    Finish authentication and Register user

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 // import { Container } from './styles';
 
+import api from '~/services/api';
+
+
 export default function Dashboard() {
+  api.get('appointments');
+
   return (
     <h6>Dashboard</h6>
   );
--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -1,26 +1,71 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { signInSuccess, signFailure } from './actions';
 import history from '~/services/history';
 import api from '~/services/api';
-import { signInSuccess } from './actions';
+import { toast } from 'react-toastify';
 
 export function* signIn({ payload }){
-    const { email, password } = payload;
+    try {
 
-    const response = yield call(api.post, '/sessions', {
-        email,
-        password,
-    });
+        const { email, password } = payload;
 
-    const { token, user } = response.data;
+        const response = yield call(api.post, '/sessions', {
+            email,
+            password,
+        });
 
-    if (!user.provider) {
-        console.tron.error('Usuário não é prestador');
-        return;
+        const { token, user } = response.data;
+
+        if (!user.provider) {
+           toast.error('Usuário não é prestador');
+           return;
+        }
+
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+
+        yield put(signInSuccess(token, user));
+
+        history.push('/dashboard');
+
+    } catch (error) {
+        toast.error('Falha na autenticação, verifique os seus dados.');
+        yield put(signFailure());
     }
-
-    yield put(signInSuccess(token, user));
-
-    history.push('/dashboard');
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }){
+    try {
+
+        const { name, email, password } = payload;
+
+        yield call(api.post, '/users', {
+            name,
+            email,
+            password,
+            provider: true,
+        });
+
+        history.push('/');
+
+    } catch (error) {
+        toast.error('Falha no cadastro, verifique os seus dados!');
+
+        yield put(signFailure());
+    }
+}
+
+export function setToken({ payload }){
+    if(!payload) return;
+
+    const { token } = payload.auth;
+
+    if(token){
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+    }
+}
+
+export default all([
+    takeLatest('persist/REHYDRATE', setToken),
+    takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+    takeLatest('@auth/SIGN_UP_REQUEST', signUp)
+]);
